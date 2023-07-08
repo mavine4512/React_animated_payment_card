815e6860e6634fd4d057dfc05e1bcc29f8a33589
mavine4512 <mavineoyugi@gmail.com>
2023-07-08 18:39:59 +0300

    payment card ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Cards from "react-credit-cards";
+import "react-credit-cards/es/styles-compiled.css";
+import "./App.css";
 
 function App() {
+  const [number, setNumber] = useState("");
+  const [name, setName] = useState("");
+  const [expiry, setExpiry] = useState("");
+  const [cvc, setCvc] = useState("");
+  const [focus, setFocus] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="formDiv">
+      <div className="creditCard">
+        <Cards
+          number={number}
+          name={name}
+          expiry={expiry}
+          cvc={cvc}
+          focused={focus}
+        />
+      </div>
+      <form>
+        <div>
+          <input
+            type="tel"
+            name="number"
+            style={{ width: "90%" }}
+            placeholder="Card Number"
+            value={number}
+            onChange={(e) => setNumber(e.target.value)}
+            onFocus={(e) => setFocus(e.target.name)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="name"
+            style={{ width: "90%" }}
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            onFocus={(e) => setFocus(e.target.name)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            name="Expiry"
+            style={{ width: "41%" }}
+            placeholder="MM/YY Expiry"
+            value={expiry}
+            onChange={(e) => setExpiry(e.target.value)}
+            onFocus={(e) => setFocus(e.target.name)}
+          />
+          <input
+            type="tel"
+            style={{ width: "41%" }}
+            name="cvc"
+            placeholder="CVC"
+            value={cvc}
+            onChange={(e) => setCvc(e.target.value)}
+            onFocus={(e) => setFocus(e.target.name)}
+          />
+        </div>
+        <div>
+          <input type="submit" value="Validate"></input>
+        </div>
+      </form>
     </div>
   );
 }
